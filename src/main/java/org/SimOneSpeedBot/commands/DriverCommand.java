01dbfe837a972c14402b46d51ce9a3da6fff9e17
Simo <145960599+ScalabrinSimone@users.noreply.github.com>
2026-01-04 21:00:51 +0100

    Aggiornata UI

--- src/main/java/org/SimOneSpeedBot/commands/DriverCommand.java
+++ src/main/java/org/SimOneSpeedBot/commands/DriverCommand.java
@@ -21,6 +21,7 @@ public class DriverCommand implements Command {
                                 🏁 Inserisci il nome del pilota...
                                 
                                 ℹ️ Puoi scrivere nome e cognome, cognome nome oppure cognome, sconsigliato se esistono piú piloti con lo stesso cognome (es: Verstappen ritorna il padre; max verstappen, Verstappen Max ritorna il figlio).
+                                Se vieni dal menu puoi continuare a scrivere i nomi dei piloti, per smettere l'inserimento, premere il pulsante "Back To Race Menu".
                                 """;
 
     public DriverCommand(TelegramClient client, Map<Long, String> userStates) {
